fd43fe995506cf71c33ae9dcb0e7203f447d42ef
A.E.L <alf.loeffler@icloud.com>
2021-10-18 17:09:25 +0200

    summary data from 30 stations on NO2 and WG added

--- GlimpseBW_data.R
+++ GlimpseBW_data.R
@@ -2,12 +2,33 @@ library(tidyverse)
 library(readxl)
 url_Rdat<- "~/Documents/Luftqualitaet/Daten/BW_Rdat/"
 list.files(path=url_Rdat)
-load(file.path(url_Rdat,"ALB_tbl.RData"))
+load(file.path(url_Rdat,"BW_list_tbl.RData"))
+summary(BW_list_tbl)
+BW_list_tbl %>% head(30)
+"NO2" %in% (names(BW_list_tbl$Alb))
+comp_detect <- function(df,cmp) {
+  exst<- cmp %in% names(df)
+  return(exst)
+}
+comp_detect(BW_list_tbl$Alb,"NO2")
+BW_list_tbl$Alb%>% exists(names(.))
 load(file.path(url_Rdat,"BW.RData"))
-summary(ALB_tbl)
 summary(BW.all_data)
-BW.all_data$Alb%>%summary()
-ALB_data<- BW.all_data$Alb
-ALB_data$Alb.O3%>% head()
-ALB_data%>% map(function(x){~.[[}
+
+BW_mean_median_NO2<-map_dbl(BW_list_tbl , function(x) ( ifelse (comp_detect(x,"NO2"),
+  mean(x$NO2,na.rm=TRUE)-
+    median(x$NO2,na.rm=TRUE),NA))
+)
+BW_var_NO2 <- map_dbl(BW_list_tbl, function(x) (ifelse (comp_detect (x,"NO2"),
+  var(x$NO2,na.rm=TRUE),NA)))
+BW_mean_WG <- map_dbl(BW_list_tbl,function(x) (ifelse (comp_detect (x,"WG"),
+                                                       mean(x$WG,na.rm=TRUE),NA )))
+
+BW_var_WG <-map_dbl(BW_list_tbl,function(x) { ifelse(comp_detect (x,"WG"),var(x$WG,na.rm=TRUE),NA) })
+
+
+
+
+
+
 
